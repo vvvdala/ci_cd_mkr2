c3cce59b44298f2d98b16219c3c98e426e03b027
Veronka <veronkievipad@gmail.com>
2024-05-31 13:18:34 +0300

    test for category_list

--- books/tests.py
+++ books/tests.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 import pytest
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
-from .models import Book
+from .models import Book, Category
+from django.db.models import Count
 
 
 @pytest.mark.django_db
@@ -21,5 +22,25 @@ def test_main_view_books(client):
     assert response.context['books'].count() == 1
 
 
+@pytest.mark.django_db
+def test_category_list_view(client):
+    category1 = Category.objects.create(name='Category 1')
+    category2 = Category.objects.create(name='Category 2')
 
+    book1 = Book.objects.create(title='Book 1', description='Description 1', publication_date='2024-05-31', price=10.99)
+    book2 = Book.objects.create(title='Book 2', description='Description 2', publication_date='2024-05-31', price=10.99)
+
+    book1.categories.add(category1)
+    book2.categories.add(category1, category2)
+
+    response = client.get(reverse('category_list'))
+
+    assert response.status_code == 200
+
+    assert 'categories' in response.context
+
+    annotated_categories = response.context['categories']
+    assert annotated_categories.count() == 2
+    assert annotated_categories[0].book_count == 2
+    assert annotated_categories[1].book_count == 1
 
