682d1bc40807bf71e3f1af14db810f61dee61fce
Veronka <veronkievipad@gmail.com>
2024-05-31 12:33:11 +0300

    main view

--- books/views.py
+++ books/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from .models import Book
 
 # Create your views here.
+def main(request):
+    latest_books = Book.objects.order_by('-id')[:5]
+    return render(request, 'main.html', {'books': latest_books})
