29ce9c5320fd8c7f56213d2e83fdae621c81eefb
Veronka <veronkievipad@gmail.com>
2024-05-31 12:39:33 +0300

    added model Category

--- books/models.py
+++ books/models.py
@@ -9,6 +9,13 @@ class Author(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
@@ -16,6 +23,7 @@ class Book(models.Model):
     publication_date = models.DateField()
     cover_image = models.ImageField(upload_to='book_covers/')
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='books')
 
     def __str__(self):
         return self.title
