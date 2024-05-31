cb3cf85eb5cf7578a7708671ade3a7bb323253ee
Veronka <veronkievipad@gmail.com>
2024-05-31 12:43:35 +0300

    added template category_list

--- books/views.py
+++ books/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Book
+from django.db.models import Count
+from .models import Book, Category
 
 # Create your views here.
 def main(request):
     latest_books = Book.objects.order_by('-id')[:5]
     return render(request, 'main.html', {'books': latest_books})
+
+def category_list(request):
+    categories = Category.objects.annotate(book_count=Count('books'))
+    return render(request, 'category_list.html', {'categories': categories})
